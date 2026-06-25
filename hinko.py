efcfc32e6bd0ece30a4448f54a5834fcff119faf
ramin <138068706+raminparsa@users.noreply.github.com>
2026-06-26 01:58:44 +0900

    Updatefthn hinko.py

--- hinko.py
+++ hinko.py
@@ -10,7 +10,7 @@ mk..hj
 abi = [{pokop
     "inputs":[{"name":"value","type":"uint256"}],
     "name":"setValue",fghfgh
-    "outputs":fgh h[],fgh
+    "outputs"fth:fgh h[],fgh
     "stateMutability":"nonpayable",
     "type":"function"
 }]ertnkiljytfghhhhdr
